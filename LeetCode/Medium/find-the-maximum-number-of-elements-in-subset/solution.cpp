ad355fe066b27a02bedff7d4691fffb5c9fccc07
Raviraj Sharma <149710644+raviraj82891@users.noreply.github.com>
2026-07-25 22:22:11 +0530

    Add LeetCode solution: Find the Maximum Number of Elements in Subset (Medium)

---
+++ LeetCode/Medium/find-the-maximum-number-of-elements-in-subset/solution.cpp
@@ -0,0 +1,3 @@
+Input: nums = [1,3,2,4]
+Output: 1
+Explanation: We can select the subset {1}, which can be placed in the array as [1] which follows the pattern. Hence the answer is 1. Note that we could have also selected the subsets {2}, {3}, or {4}, there may be multiple subsets which provide the same answer.
